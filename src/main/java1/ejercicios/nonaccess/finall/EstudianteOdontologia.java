06202cdd860bbc869f5d8ccbef2b7b28f6808232
Brandon Gavilanez <stalynjg2002@gmail.com>
2021-09-10 19:59:13 -0500

    Modificador final

---
+++ src/main/java1/ejercicios/nonaccess/finall/EstudianteOdontologia.java
@@ -0,0 +1,14 @@
+package ejercicios.nonaccess.finall;
+
+public class EstudianteOdontologia extends EstudianteMedicina {
+
+//No se puede sobreescribir un metodo final del padre
+	@Override
+	protected void realizarPracticas() {
+		System.out.println("Realiza 100 horas de practicas");
+	}
+
+	protected void realizarPasantias() {
+		System.out.println("Realizar 200 horas de pasantias");
+	}
+}
